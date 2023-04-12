b4a33b414de1a6c9848b4e4fe63c73e97d543013
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-12 16:21:06 +0900

    Firebase State Persistence

--- lib/router.dart
+++ lib/router.dart
@@ -5,6 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:reddit_tutorial/features/auth/screens/login_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
+import 'features/home/screen/home_screen.dart';
+
 final loggedOutRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: LoginScreen())
+});
+
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen()),
 });
